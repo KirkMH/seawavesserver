6a6780f6371e3683929ae3725bf46103104473d3
Kirk Hilario <kirkhilario@gmail.com>
2022-07-21 22:05:55 +0800

    added seconds to boat record timestamp.

--- static/js/formatter.js
+++ static/js/formatter.js
@@ -1,10 +1,12 @@
-function formatDate(date) {
+function formatDate(date, withSec=false) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
+    var secs = date.getSeconds();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ampm;
+    secs = secs < 10 ? '0'+secs : secs;
+    var strTime = hours + ':' + minutes + (withSec) ? (':' + secs) : '' + ampm;
     return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
 }
