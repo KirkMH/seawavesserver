cefbb965c9fafa0d7dd704bc9a02cabe661ed246
Kirk Hilario <kirkhilario@gmail.com>
2022-07-26 16:41:54 +0800

    bug fixes

--- reporting/views.py
+++ reporting/views.py
@@ -3,7 +3,6 @@ from monitoring.models import Boat
 
 def route_view(request):
     context = {
-        'boats': Boat.objects.all(),
-
+        'boats': Boat.objects.all().order_by('name'),
     }
     return render(request, 'reporting/route.html', context)
